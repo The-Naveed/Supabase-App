0ea9a3224885853b54ffbb2e3ab9fd0fe8376c24
Your Name <you@example.com>
2025-01-16 14:42:48 +0500

    C R U D Complete

--- app.js
+++ app.js
@@ -1,25 +1,81 @@
-const projectURL = "https://jmcchntnmrblzbjeyfyy.supabase.co";
-const projectAPI = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptY2NobnRubXJibHpiamV5Znl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4NzAyNDcsImV4cCI6MjA1MjQ0NjI0N30.1a63qBpT0il6477fYM-m1J1U9YGEGH05J2Y6RGeIMGY";
-export const supabaseClient = supabase.createClient(projectURL, projectAPI);
+const sup = supabase.createClient("https://jmcchntnmrblzbjeyfyy.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptY2NobnRubXJibHpiamV5Znl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4NzAyNDcsImV4cCI6MjA1MjQ0NjI0N30.1a63qBpT0il6477fYM-m1J1U9YGEGH05J2Y6RGeIMGY")
 
 
-const addData = async () => {
+// Creating C R U D 
+
+// C R E A T E
+const addData = async _ => {
+    const info = {
+        firstName: "Naveed",
+        lastName: "Ahmed",
+        age: 25,
+        country: "Pakistan",
+        city: "Karachi"
+    };
     try {
-        const { data, error } = await supabase.auth.signUp({
-            email: 'naveed@gmail.com',
-            password: '12345678',
-        });
+        const { data, error } = await sup
+            .from('data')
+            .insert(info)
+            .select()
 
-        if (error) {
-            console.error('Error signing up:', error.message);
-        } else {
-            console.log('Sign-up successful:', data);
-        }
+        console.log(data);
+        console.log(error);
+
     } catch (error) {
-        console.error('Unexpected error:', error);
+        console.log(error);
+    }
+}
+// addData();
+
+// R E A D
+const readata = async _ => {
+    try {
+        const { data, error } = await sup
+            .from('data')
+            .select()
+
+        console.log(data);
+        console.log(error);
+
+    } catch (error) {
+        console.log(error);
     }
 }
 
+// readata()
 
-addData()
-window.addData = addData
+// U P D A T E
+const updateData = async _ => {
+    try {
+        const { data, error } = await sup
+            .from('data')
+            .update({ country: 'Australia' })
+            .eq('id', 13)
+            .select()
+
+        console.log(data);
+        console.log(error);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// updateData()
+
+// D E L E T E
+const delData = async _ => {
+    try {
+        const { data, error } = await sup
+            .from('data')
+            .delete()
+            .eq('id', 12)
+            .select()
+
+        console.log(data);
+        console.log(error);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// delData()
